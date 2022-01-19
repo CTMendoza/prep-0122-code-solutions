04b35abfdd31d24ae473585bc97c52a4c9e6afcf
CTMENDOZA <tiptop1112@gmail.com>
2022-01-18 16:52:08 -0800

    Added properties to exisiting object via dot notation

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -1,19 +1,14 @@
 const person = {
   firstName: 'Chris',
   lastName: 'Mendoza',
-  hobby: 'Playing Video Games',
-  job: 'Accountant',
-  previousJob: 'Billing Clerk'
+  hobby: 'Playing Video Games'
 };
 
-console.log('firstName: ', person.firstName, 'lastName: ', person.lastName, 'hobby: ', person.hobby);
-
+console.log(person);
 var fullName = person.firstName + ' ' + person.lastName;
-
 console.log('The persons full name is ', fullName);
-
+person.job = 'Accountant';
 console.log('The person"s current job is  ', person.job);
-
+person.previousJob = 'Billing Clerk';
 console.log('The person"s previous job was  ', person.previousJob);
-
 console.log(person);
